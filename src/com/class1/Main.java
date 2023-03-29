d7ec24501e5ed91c911241212a04134ff87a01b4
nguyenhuudung201 <115616484+nguyenhuudung201@users.noreply.github.com>
2023-03-29 11:44:30 +0700

    up java code

--- src/com/class1/Main.java
+++ src/com/class1/Main.java
@@ -2,18 +2,7 @@ package com.class1;
 
 public class Main {
     public static void main(String[] args) {
-       /* Circle circle = new Circle();
-        circle.inputData();
-        circle.getArea();
-        circle.display();
-        Rectangle rectangle = new Rectangle();
-        rectangle.inputData();
-        rectangle.getArea();
-        rectangle.display();
-        Triangle triangle = new Triangle();
-        triangle.inputData();
-        triangle.getArea();
-        triangle.display();*/
+
         Shape shape[]= new  Shape[3];
         shape[0] = new Circle();
         shape[1] = new Rectangle();
